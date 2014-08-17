0675c6d4107bb31763003bd2d2988a8780f105e1
rikardjonsson <larsrikardjonsson@gmail.com>
2014-08-17 16:16:08 +0200

    removed s

--- neoconnector.js
+++ neoconnector.js
@@ -7,7 +7,7 @@ function searchFragrance() {
   /*$("#result").val('');*/
   var fragrenceName = $("#searchFragInput").val();
   var cypherQuery = "MATCH (p:Perfume)-[:smell]->(n:note)<-[:smell]-(p2:Perfume) WHERE p.title = '"+fragrenceName+"' AND NOT p=p2 RETURN p2.title AS name, p2.brand, count(*) AS similarity ORDER BY similarity DESC limit 999";
-  var serverURL = "https://app27890640:itRYoUIUabfBj7zVvPrD@app27890640.sb02.stations.graphenedb.com:24789/db/data";
+  var serverURL = "http://app27890640:itRYoUIUabfBj7zVvPrD@app27890640.sb02.stations.graphenedb.com:24789/db/data";
   $.ajax({
     type:"POST",
     url: serverURL + "/cypher",
